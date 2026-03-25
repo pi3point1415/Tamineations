4934712dcc995d788e00481245b5492802d6e8ef
hhall314 <hhall314@mit.edu>
2026-03-25 12:53:22 -0400

    Start standalone branch

--- src/client/kotlin/com/pi3point14/tamineations/client/TamineationsClient.kt
+++
@@ -1,9 +0,0 @@
-package com.pi3point14.tamineations.client
-
-import net.fabricmc.api.ClientModInitializer
-
-class TamineationsClient : ClientModInitializer {
-
-    override fun onInitializeClient() {
-    }
-}
