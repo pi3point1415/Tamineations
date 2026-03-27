71288a7d4f61eb2051e9ea20ffe13cb0c0ad2041
hhall314 <hhall314@mit.edu>
2026-03-26 20:19:06 -0400

    New base terminal project

--- composeApp/src/commonMain/kotlin/com/pi3point14/tamineations/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.pi3point14.tamineations
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- composeApp/src/jsMain/kotlin/com/pi3point14/tamineations/Platform.js.kt
+++
@@ -1,7 +0,0 @@
-package com.pi3point14.tamineations
-
-class JsPlatform : Platform {
-    override val name: String = "Web with Kotlin/JS"
-}
-
-actual fun getPlatform(): Platform = JsPlatform()
